bddcec3cdbbaed070384f8a6a7e9e37f0a9fb900
egor <mikhaylov2013a@yandexx.ru>
2022-11-17 14:49:19 +0300

    v1.01

---
+++ main.py
@@ -0,0 +1,32 @@
+import random
+povezlo_povezlo = 0
+ne_povezlo_ne_povezlo = 0
+povezlo_povezlo_2 = 0
+ne_povezlo_ne_povezlo_2 = 0
+a = 1000000
+for j in range(a):
+    spisok = [1, 2, 3]
+    winn = random.choice(spisok)
+    vibor = random.choice(spisok)
+    for i in spisok:
+        if i != winn and i != vibor:
+            del spisok[spisok.index(i)]
+            break
+    vibor_2 = random.choice(spisok)
+    while vibor == vibor_2:
+        vibor_2 = random.choice(spisok)
+    if vibor_2 == winn:
+        povezlo_povezlo += 1
+    else:
+        ne_povezlo_ne_povezlo += 1
+print(povezlo_povezlo / a * 100, "%", ' menyaem', sep='')
+spisok = [1, 2, 3]
+for j in range(a):
+    spisok = [1, 2, 3]
+    winn = random.choice(spisok)
+    vibor = random.choice(spisok)
+    if vibor == winn:
+        povezlo_povezlo_2 += 1
+    else:
+        ne_povezlo_ne_povezlo_2 += 1
+print(povezlo_povezlo_2 / a * 100, "%", ' ne menyaem', sep='')
